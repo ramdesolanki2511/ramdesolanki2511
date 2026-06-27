7595a71c72fc5900423f8c130736020d9323c9f0
ramde solanki <ramdesolanki2511@gmail.com>
2026-06-27 17:35:06 +0530

    home page update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  // output: "export", // comment this line while development
+  assetPrefix: "/",
+  // exportTrailingSlash: true, // comment this line while development
+  basePath: "",
+  // trailingSlash: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: true
+      }
+    ];
+  }
 };
-
-export default nextConfig;
